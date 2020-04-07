b3f4b71f89f1722b38a69cb190cbdefa7aa20f15
yaroslavlebid <y.lebid@outlook.com>
2020-04-07 17:11:19 +0300

    review-slider

--- js/review-slider.js
+++ js/review-slider.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($){
 	//create the slider
-	$('.cd-testimonials-wrapper').flexslider({
-		selector: ".cd-testimonials > li",
+	$('.review-slide').flexslider({
+		selector: ".review-content > li",
 		animation: "slide",
 		controlNav: false,
 		slideshow: false,
 		smoothHeight: true,
 		start: function(){
-			$('.cd-testimonials').children('li').css({
+			$('.review-content').children('li').css({
 				'opacity': 1,
 				'position': 'relative'
 			});
